717ed19a007fa90076fadbf30994673c8a7a2907
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-08-28 00:40:24 -0300

    wip

--- src/sthali_cli/generate_project.py
+++ src/sthali_cli/generate_project.py
@@ -1,4 +1,16 @@
+from subprocess import call
 from cookiecutter.main import cookiecutter
 
 
-cookiecutter("git+ssh://git@github.com/jhunufernandes/sthali-cli")
+# cookiecutter("https://github.com/jhunufernandes/sthali-project-template")
+cookiecutter("https://github.com/jhunufernandes/sthali-cli")
+
+project_name = "sthali-batatassauro"
+
+
+def move_content():
+    call(["mv", f"./{project_name}/*", f"./{project_name}/.*", "."])
+
+
+def delete_directory():
+    call(["rm", "-rf", "./{project_name}"])
